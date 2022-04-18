a861605fcef76e52e572362fc9d4953ef54c47eb
swostika61 <swostikalamichhane61@gmail.com>
2022-04-18 22:50:17 +0545

    handle the no items in cart

---
+++ src/components/NoCartItem.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoCartItem = () => {
+  return (
+    <div className="px-6 py-2 text-center font-medium text-zinc-600 text-xl">No items available!</div>
+  )
+}
+
+export default NoCartItem
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,26 +1,25 @@
 import React from "react";
+import NoCartItem from "../components/NoCartItem";
 import PriceCard from "../components/PriceCard";
 import { CartState } from "../context/Context";
 
 const Cart = () => {
   const { state, dispatch } = CartState();
   // const [total, setTotal]= useState()
-  const totalCartLength:number|undefined = state?.cart.length;
-  return (
-    { totalCartLength > 0  ?
-    (<div className="grid grid-cols-2 px-6">
-      <>
+  const totalCartLength: number = state?.cart.length ?? 0;
+  return totalCartLength > 0 ? (
+    <div className="grid grid-cols-2 px-6">
+      <div>
         {state?.cart.map((prod) => {
           return <div>{prod.title}</div>;
         })}
-      </>
-      <div>
-        <PriceCard>
-          Total items :{state?.cart}
-        </PriceCard>
       </div>
-    </div>):<div>none</div>}
-      
+      <div>
+        <PriceCard>Total items :{state?.cart.length}</PriceCard>
+      </div>
+    </div>
+  ) : (
+    <NoCartItem />
   );
 };
 
