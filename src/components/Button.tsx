6bf4f9c39ac85b29bf646669c97ffc9ff92f73c8
swostika61 <swostikalamichhane61@gmail.com>
2022-04-19 19:30:30 +0545

    changes in button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 interface IButtonProps {
   hcolor?: string;
+  mleft?: string;
   onclick?:()=>void
 }
-const Button: React.FC<IButtonProps> = ({ hcolor, children,onclick }) => {
+const Button: React.FC<IButtonProps> = ({ hcolor,mleft, children,onclick }) => {
   return (
     <button
       onClick={onclick}
-    className={`bg-white ${hcolor} hover:text-white border border-solid border-zinc-800 w-1/3 lg:w-full py-2`}>
+    className={`bg-white ${hcolor} ${mleft} hover:text-white text-gray-800 border border-solid border-zinc-800 w-1/3 lg:w-full py-2`}>
     {children}
   </button>
   );
