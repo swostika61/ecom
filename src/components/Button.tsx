a38a574b31373dec77e3b2c6caa942e19b06cd11
swostika61 <swostikalamichhane61@gmail.com>
2022-04-19 07:40:45 +0545

    chnages the button style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ const Button: React.FC<IButtonProps> = ({ hcolor, children,onclick }) => {
   return (
     <button
       onClick={onclick}
-    className={`bg-white ${hcolor} hover:text-white border border-solid border-grey w-1/3 lg:w-full py-2`}>
+    className={`bg-white ${hcolor} hover:text-white border border-solid border-zinc-800 w-1/3 lg:w-full py-2`}>
     {children}
   </button>
   );
