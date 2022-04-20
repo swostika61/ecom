840cb3cdeb45745cfdf34e0623ceb20c07cda9c4
swostika61 <swostikalamichhane61@gmail.com>
2022-04-20 10:59:56 +0545

    added the proceed button

--- src/components/PriceCard.tsx
+++ src/components/PriceCard.tsx
@@ -4,7 +4,7 @@ interface IPriceCardProps {
 }
 const PriceCard: React.FC<IPriceCardProps> = ({ children }) => {
   return (
-    <div className="max-w-sm rounded-sm overflow-hidden shadow-lg flex justify-center bg-slate-400 py-3 " style={{minHeight:"415px"}}>
+    <div className="max-w-sm  rounded-lg overflow-hidden shadow-lg flex justify-center dark:bg-gray-800 py-3 text-white" style={{minHeight:"415px"}}>
       {children}
     </div>
   );
