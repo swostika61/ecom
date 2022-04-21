89a56fbc87e3355a8dba3977370005d5a1f522b6
swostika61 <swostikalamichhane61@gmail.com>
2022-04-21 09:33:18 +0545

    added the delete functionality annd some changes in style

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { CartState } from "../context/Context";
 import Button from "./Button";
 interface ICardItemProps {
   product: IProducts;
 }
 const CartItem: React.FC<ICardItemProps> = ({ product }) => {
+  const { state, dispatch } = CartState()
   return (
     <div className="block mb-3 p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 text-white">
       <div className="flex items-center">
@@ -18,8 +20,12 @@ const CartItem: React.FC<ICardItemProps> = ({ product }) => {
       </div>
       {/* buttons  */}
       <div className="flex mt-3">
-        <Button hcolor="hover:bg-blue-500">Add</Button>
-        <Button hcolor="hover:bg-red-500" mleft="ml-3">Delete</Button>
+        <Button hcolor="hover:bg-red-500" mleft="ml-3" onclick={() => {
+                            dispatch?.({
+                              type: "REMOVE_FROM_CART",
+                              payload: product,
+                            });
+                          }}>Delete</Button>
       </div>
     </div>
   );
